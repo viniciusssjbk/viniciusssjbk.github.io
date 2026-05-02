752b94ffb50559f35c8e47c93977ebf649ea3d36
viniciusssjbk <viniciusssjbk@gmail.com>
2026-05-02 17:38:18 -0300

    deixando o site mais dinamico e responsivo

---
+++ fundo.js
@@ -0,0 +1,18 @@
+VANTA.BIRDS({
+  el: "#fundo",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0x0,
+  color1: 0xd1ff,
+  color2: 0x1805a7,
+  colorMode: "lerpGradient",
+  birdSize: 1.10,
+  speedLimit: 6.00,
+  separation: 29.00,
+  alignment: 28.00
+})
--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-VANTA.GLOBE({
-  el: "#fundo",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: 0x31be41,
-  color2: 0x186296,
-  size: 0.90,
-  backgroundColor: 0x0
-})
+
+var animatedSobre = false;
+var animatedProjetos = false;
+var animatedContatos = false;
+
+window.addEventListener('scroll', () => {
+    const posicaoScroll = window.scrollY;
+    const setaTopo = document.getElementById('voltar_topo');
+    const sobre = document.getElementById('about');
+    const projetos = document.getElementById('projects');
+    const contatos = document.getElementById('contact');
+
+
+    if (posicaoScroll > 150) {
+        setaTopo.style.display = 'flex';
+        setaTopo.style.opacity = '0.7';
+    } else {
+        setaTopo.style.display = 'none';
+        setaTopo.style.opacity = '0';
+    }
+
+  
+    if (posicaoScroll >= 350 && posicaoScroll < 850 && !animatedSobre) {
+        sobre.style.animation = 'fadeIn 2s forwards';
+        animatedSobre = true;
+    }
+
+    else if (posicaoScroll >= 850 && posicaoScroll < 1950 && !animatedProjetos) {
+        projetos.style.animation = 'fadeIn 2s forwards';
+        animatedProjetos = true;
+    }
+
+    else if (posicaoScroll >= 1950 && !animatedContatos) {
+        contatos.style.animation = 'fadeIn 2s forwards';
+        animatedContatos = true;
+    }
+});
