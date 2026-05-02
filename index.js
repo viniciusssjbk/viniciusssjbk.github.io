c6879aa6e3fdb9927c74a51e88a94d416d38f74a
viniciusssjbk <viniciusssjbk@gmail.com>
2026-05-02 03:39:14 -0300

    Versão 1.1 do portiforio

---
+++ index.js
@@ -0,0 +1,14 @@
+VANTA.GLOBE({
+  el: "#fundo",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x31be41,
+  color2: 0x186296,
+  size: 0.90,
+  backgroundColor: 0x0
+})
